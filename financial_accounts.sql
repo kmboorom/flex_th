01dc6bae121ee46ca92e7b760dbc2dd205c01c1d
kmboorom <79178098+kmboorom@users.noreply.github.com>
2025-02-01 11:49:07 -0500

    Create financial_accounts.sql

---
+++ financial_accounts.sql
@@ -0,0 +1,6 @@
+--init insert statements
+
+INSERT INTO financial_accounts (account_id, parent_id, name, value) VALUES
+('A1', NULL, 'ASSETS', 13318970.87),
+('L1', NULL, 'LIABILITIES', 1025016.99),
+('E1', NULL, 'EQUITY', 12399101.55)
