46efe96093a9809d951fe7e23d6230ad2bc18047
kmboorom <79178098+kmboorom@users.noreply.github.com>
2025-02-01 13:03:09 -0500

    Update financial_accounts.sql

--- financial_accounts.sql
+++ financial_accounts.sql
@@ -3,7 +3,7 @@
 INSERT INTO financial_accounts (account_id, parent_id, name, value) VALUES
 ('A1', NULL, 'ASSETS', 13318970.87),
 ('L1', NULL, 'LIABILITIES', 1025016.99),
-('E1', NULL, 'EQUITY', 12399101.55)
+('E1', NULL, 'EQUITY', 12399101.55),
 
 --Current Assets, making the heirarchy between the accounts
 ('A2', 'A1', 'Current Assets', 13300233.24),
