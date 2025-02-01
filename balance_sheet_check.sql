4e24023342c04b075b68d4a07f62be6cf308da4e
kmboorom <79178098+kmboorom@users.noreply.github.com>
2025-02-01 12:12:07 -0500

    Update balance_sheet_check.sql

--- balance_sheet_check.sql
+++ balance_sheet_check.sql
@@ -9,8 +9,8 @@ select
 --will return true if A != L+SE which will fail the test
 sum(
   case
-    when name = ('ASSETS') then value
-    else value * -1
+    when account_id = ('A1') then value
+    when account_id in ('L1', 'E1') then value * -1
   end as flag
 ) > 0 
 
